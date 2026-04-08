21031efc38842db94e79be5f5e75d671d1bba518
pranitha <pranithaboppanapally@gmail.com>
2026-04-08 22:38:15 +0530

    fix db connection final

--- configs/db.js
+++ configs/db.js
@@ -6,6 +6,7 @@ const db = mysql.createConnection({
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT,
+  connectTimeout: 10000,
 });
 
 db.connect((err) => {
