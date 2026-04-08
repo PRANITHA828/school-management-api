f6bfd6d9e3c83c8de91723edd8d11f19f7655647
pranitha <pranithaboppanapally@gmail.com>
2026-04-08 20:56:07 +0530

    Ready for deployment

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 require("./configs/db");
 
+const PORT = process.env.PORT || 5000;
+
 const schoolRoutes = require("./routes/schoolroutes");
 
 app.use(express.json());
@@ -9,7 +11,7 @@ app.use("/", schoolRoutes);
 
 
 
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
