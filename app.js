cdd5e2dc10cbeb5635017b20594515cca217c709
pranitha <pranithaboppanapally@gmail.com>
2026-04-09 17:56:03 +0530

    connect routes

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const schoolRoutes = require("./routes/schoolroutes");
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -7,7 +9,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+app.use("/", schoolRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Server working ");
+});
 
 
 app.listen(PORT,"0.0.0.0", () => {
